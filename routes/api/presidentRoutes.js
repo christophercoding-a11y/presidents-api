c9b20c637e2687cbc5fa9f549c3439f74010224b
christophercodinga11y <christopherjack14@gmail.com>
2025-04-11 14:07:44 -0500

    adding some styling

---
+++ routes/api/presidentRoutes.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const router = express.Router()
+const axios = require('axios')
+
+let count
+axios.get('https://api.sampleapis.com/presidents/presidents')
+.then(res => count = res.data.length)
+
+router.get('/', (req, res)=>{
+    const url = 'https://api.sampleapis.com/presidents/presidents'
+
+    axios.get(url).then(response => {
+        res.render('pages/presidents', {
+            title: "President's",
+            name: "All President's",
+            data: response.data,
+            path: 'president'
+        })
+    })
+})
+
+router.get('/:id', (req, res)=> {
+    const id = req.params.id
+
+    const url = `https://api.sampleapis.com/presidents/presidents/${id}`
+
+    axios.get(url).then(resp => {
+
+        const data = resp.data
+        res.render('pages/presidentSingle', {
+            title: data.title,
+            name: data.name,
+            data: data,
+            path: 'president',
+            count
+        })
+    })
+})
+
+
+module.exports = router
+
---
+++ routes/router.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const axios = require('axios')
+const router = express.Router()
+
+router.use(express.static('public'))
+
+const presidentRoutes = require('./api/presidentRoutes')
+router.use('/presidents', presidentRoutes)
+
+router.get('/', (req, res)=> {
+    let randomPresident = {}
+    axios.get('https://api.sampleapis.com/presidents/presidents')
+    .then(resp => {
+        randomPresident = resp.data[Math.floor(Math.random()* resp.data.length)]
+        res.render('pages/home', {
+            title: "President's Home page",
+            name: "President's Website",
+            president: randomPresident
+        })
+    })
+})
+
+
+
+module.exports = router
